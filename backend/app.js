bd1714e2722fae9b9c3b12dfda42519f01886e5d
Diana Cruz <dragonpumperex@gmail.com>
2019-12-18 21:12:36 -0600

    muere en login, CORS

--- backend/app.js
+++ backend/app.js
@@ -52,15 +52,15 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 const whitelist = [
   "https://app.netlify.com/sites/suspicious-villani-0c139b",
