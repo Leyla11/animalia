7ab2eb55e8aa37d10b2ff08f5354c9ba5caa3be1
Diana Cruz <dragonpumperex@gmail.com>
2019-12-19 13:00:09 -0600

    ya

--- frontend/src/components/shelter/AllShelter.js
+++ frontend/src/components/shelter/AllShelter.js
@@ -95,7 +95,6 @@ export default class Shelter extends Component {
     const { user } = this.state;
     return (
       <Layout history={this.props.history}>
-       
         <div className="section">
           <div className="container">
             {/* <p className="has-text-centered category-center">
@@ -109,60 +108,57 @@ export default class Shelter extends Component {
                 ref={e => (this.mapContainer = e)}
               /> */}
               <div className="column is-5 data">
-              <div>
-                </div>
-          {shelters.map(shelter => (
-            <div>{shelter.place}</div>
-          
-                <div className="card">
-                  <header className="card-header">
-                    <p className="card-header-title card-text">
-                      {shelter.place}
-                    </p>
-                  </header>
-                  <div className="card-content">
-                    <div className="content">
-                      <p>Adress: {shelter.address}</p>
-                      <p>
-                        Email:{" "}
-                        {shelter.email
-                          ? shelter.email
-                          : "Not Available"}
-                      </p>
-                      <p>
-                        Number:{" "}
-                        {shelter.contactNumber
-                          ? shelter.contactNumber
-                          : "Not Available"}
-                      </p>
-                      <p>
-                        Website:{" "}
-                        {shelter.website
-                          ? shelter.website
-                          : "Not Available"}
-                      </p>
-                    </div>
-                    <div className="home-button">
-                      {user.role === "USUARIO" ? (
-                        <>
-                          <NavLink exact to="/allShelter">
-                            <button className="button is-primary">
-                              Add Shelter 🐾
-                            </button>
-                          </NavLink>
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                  </div>
-                </div>
+                <div></div>
+                {shelters.map(shelter => (
+                  <>
+                    <div>{shelter.place}</div>
 
-))}
+                    <div className="card">
+                      <header className="card-header">
+                        <p className="card-header-title card-text">
+                          {shelter.place}
+                        </p>
+                      </header>
+                      <div className="card-content">
+                        <div className="content">
+                          <p>Adress: {shelter.address}</p>
+                          <p>
+                            Email:{" "}
+                            {shelter.email ? shelter.email : "Not Available"}
+                          </p>
+                          <p>
+                            Number:{" "}
+                            {shelter.contactNumber
+                              ? shelter.contactNumber
+                              : "Not Available"}
+                          </p>
+                          <p>
+                            Website:{" "}
+                            {shelter.website
+                              ? shelter.website
+                              : "Not Available"}
+                          </p>
+                        </div>
+                        <div className="home-button">
+                          {user.role === "USUARIO" ? (
+                            <>
+                              <NavLink exact to="/allShelter">
+                                <button className="button is-primary">
+                                  Add Shelter 🐾
+                                </button>
+                              </NavLink>
+                            </>
+                          ) : (
+                            <></>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ))}
               </div>
             </div>
           </div>
-          //{" "}
         </div>
       </Layout>
     );
